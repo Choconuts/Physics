a5a062480010fed48c4faf94ecbd02f6b43b5947
Choconuts <843940285@qq.com>
2022-10-01 13:01:43 +0800

    感觉还是在依靠Mask

--- cluster/match.py
+++ cluster/match.py
@@ -2,6 +2,7 @@ import torch
 import imgui
 import numpy as np
 from cluster.camera import Scene, ui, SynDataset, FocusSampler, Inputable, visualize_field
+from utils import rend_util
 
 
 def ln_var(x, sharpness=6):
@@ -10,17 +11,22 @@ def ln_var(x, sharpness=6):
     return -np.log(1 + np.exp(sharpness * x - 3))
 
 
+def mask_expect_fn(x, expect=80):
+    return torch.sigmoid(12 / expect * (x - expect * 0.6))
+
+
 class Opt(Inputable):
 
     def __init__(self, z_val=0.0):
+        self.t_val = z_val
         self.x_val = z_val
         self.y_val = z_val
         self.z_val = z_val
 
     def gui(self, label, *args, **kwargs):
-        self.changed, self.x_val = imgui.slider_float('x', self.x_val, -1.5, 1.5)
-        self.changed, self.y_val = imgui.slider_float('y', self.y_val, -1.5, 1.5)
-        self.changed, self.z_val = imgui.slider_float('z', self.z_val, -1.5, 1.5)
+        self.changed, self.t_val = imgui.slider_float('t', self.t_val, 0.2, 5.0)
+        self.changed, (self.x_val, self.y_val, self.z_val) = \
+            imgui.slider_float3('x', self.x_val, self.y_val, self.z_val, -1.5, 1.5)
 
 
 opt = Opt()
@@ -29,18 +35,45 @@ opt = Opt()
 class MatchScene(Scene):
 
     def __init__(self, n_image):
-        self.data = SynDataset(r"E:\BaiduNetdiskDownload\colmap_result", 458 // n_image, blender=False)
-        # self.data = SynDataset(r"G:\Repository\nerf-pytorch\data\nerf_synthetic\lego", 100 // n_image)
+        # self.data = SynDataset(r"E:\BaiduNetdiskDownload\colmap_result", 458 // n_image, blender=False)
+        self.data = SynDataset(r"G:\Repository\nerf-pytorch\data\nerf_synthetic\lego", 100 // n_image)
         self.focus_sampler = FocusSampler(self.data)
 
     def sample_gt_mask(self, uv, selector=None):
         return torch.ones_like(uv[..., 0]).bool()
+
+    def sample_image(self, idx, res=100):
+        if isinstance(idx, int):
+            selector = lambda arr: arr[idx:idx+1]
+        else:
+            selector = idx
+
+        pose = torch.stack(self.data.pose_all).cuda()
+        intrinsics = torch.stack(self.data.intrinsics_all).cuda()
+        s = torch.linspace(-1, 1, res).cuda()
+        uv = torch.stack(torch.meshgrid([s, s]), -1).view(-1, 2)
+        pose_selected = selector(pose)
+        uv = uv.view(1, -1, 2).expand(len(pose_selected), -1, -1)
+        uv_tex = self.uv_to_tex(uv)
+        mask = self.sample_gt_mask(uv, selector)
+        rgb = self.sample_gt(uv, selector)
+        rays_d, rays_o = rend_util.get_camera_params(uv_tex, pose_selected, selector(intrinsics))
+        rays_o = rays_o.unsqueeze(1).expand(rays_d.shape)
+        rays_d = rays_d.reshape(-1, 3)
+        rays_o = rays_o.reshape(-1, 3)
+
+        return rays_o, rays_d, rgb.view(-1, 3), mask.view(-1)
 
     def sample_neighbor(self, x, dirs, best_n=20):
         sample, gt = self.focus_sampler.scatter_sample(x)
         all_mask = sample['object_mask']
         all_rgb = gt['rgb']
         all_dirs = sample['view_dir']
+
+        all_mask[(all_dirs * dirs).sum(-1) < 0] = 0
+
+        if best_n <= 0:
+            return all_rgb, all_mask
 
         score = (all_dirs - dirs).norm(dim=-1)
         _, idx = torch.sort(score, 0)
@@ -103,30 +136,63 @@ class MatchScene(Scene):
 
         while True:
             if opt.changed:
-                best_n = 20
-                rays_o, rays_d, rgb0, mask0 = self.sample_batch(2048)
-                t = torch.linspace(0.0, 3.0, 128).cuda()[:, None]
-                x = rays_o.unsqueeze(-2) + rays_d.unsqueeze(-2) * t
-                x = x[97]
-                rays_d = rays_d[97]
-                rays_o = rays_o[97]
+                best_n = -1
+                rays_o, rays_d, rgb0, mask0 = self.sample_image(2)
+                # t = torch.linspace(0.0, 3.0, 128).cuda()[:, None]
+                # x = rays_o.unsqueeze(-2) + rays_d.unsqueeze(-2) * t
+                # x = x[97]
+                # rays_d = rays_d[97]
+                # rays_o = rays_o[97]
+                x = rays_o + rays_d * opt.t_val
 
                 rgb, mask = self.sample_neighbor(x, rays_d, best_n)
+
                 mask = mask[..., None]
                 # poses = [self.data.pose_all[i] for i in idx[:best_n, 0]]
 
                 mean_rgb = torch.sum(rgb * mask, dim=0) / mask.sum(0)
-                mean_var = torch.sum((rgb - mean_rgb) ** 2 * mask, dim=0) / mask.sum(0)
-                # mean_var = (ln_var((rgb - mean_rgb).norm(dim=-1))).mean(0)
-                # prob = torch.exp(mean_var * 2)
+                mask_exp = mask.sum(0)[..., 0]
+                mask_prob = mask_expect_fn(mask_exp)
+                mean_var = (ln_var((rgb - mean_rgb).norm(dim=-1))).mean(0)
+                prob = torch.exp(mean_var * 2)
 
-                visualize_field(x, scalars=mean_var * 10)
+                visualize_field(x, scalars=prob * mask_prob)
                 self.vis_axis()
                 # self.vis_rays(lambda x: x[idx[2:3, 0],], 20)
             yield
 
+    @ui(opt)
+    def show_heat(self):
+
+        while True:
+            if opt.changed:
+                best_n = -1
+                pose_id = 4
+                rays_o, rays_d, rgb0, mask0 = self.sample_image(pose_id, res=80)
+                t = torch.linspace(0.1, 2.5, 80).cuda()[:, None]
+                x = rays_o.unsqueeze(-2) + rays_d.unsqueeze(-2) * t
+                rgb0 = rgb0.unsqueeze(-2).expand(x.shape).reshape(-1, 3)
+
+                rgb, mask = self.sample_neighbor(x.reshape(-1, 3), rays_d.unsqueeze(-2).expand(x.shape).reshape(-1, 3), best_n)
+
+                mask = mask[..., None]
+                # poses = [self.data.pose_all[i] for i in idx[:best_n, 0]]
+
+                # mean_rgb = torch.sum(rgb * mask, dim=0) / mask.sum(0)
+                mask_exp = mask.sum(0)[..., 0]
+                mask_prob = mask_expect_fn(mask_exp)
+                # mean_var = (ln_var((rgb - rgb0).norm(dim=-1)) * mask[..., 0]).sum(0) / mask_exp
+                mean_var = (ln_var((rgb - rgb0).norm(dim=-1))).mean(0)
+                prob = torch.exp(mean_var * 2)
+                prob = prob * mask_prob
+                x = x.reshape(-1, 3)[prob > opt.t_val / 5]
+                visualize_field(x, scalars=rgb0[prob > opt.t_val / 5])
+                self.vis_axis(self.data.pose_all[pose_id:pose_id + 1])
+            yield
+
+
 scene = MatchScene(100)
-scene.show_hit()
+scene.show_heat()
 
 if __name__ == '__main__':
     pass
